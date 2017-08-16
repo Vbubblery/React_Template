b4a38762ac292286f57604501cb7325837d9ed81
Vbubblery <juncheng.zhou@u-psud.fr>
2017-08-16 15:44:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use(function(req, res) {
   Router.run(routes, req.path, function(Handler) {
